0ff73e9a327cf32cdddf9c2d4e3911087ed036f0
ambler289 <109005318+ambler289@users.noreply.github.com>
2026-08-25 09:47:18 +1200

    Add sitemap generation for search indexing

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
-    site: 'https://flowbyadapt.com',
+  site: 'https://flowbyadapt.com',
+  integrations: [sitemap()],
 });
